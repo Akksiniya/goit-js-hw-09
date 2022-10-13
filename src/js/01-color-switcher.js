443bc59fdf293408e80a1b74f2424f6aed88703e
akksinya <akksin.ya999@gmail.com>
2022-10-13 17:20:53 +0300

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+const refs = {
+    body: document.querySelector('body'),
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]'),
+};
+
+console.log(refs.buttonStart);
+console.log(refs.buttonStop);
+
+let timerId = null;
+
+refs.buttonStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+     refs.body.style.backgroundColor = getRandomHexColor();
+     refs.buttonStart.disabled = true;
+    }, 1000);
+
+});
+
+refs.buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.buttonStart.disabled = false;
+});
 
